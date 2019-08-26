741146496f1fddead86e7f81f20ebb29d43a796c
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-08-26 03:08:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int ciclos(unsigned int num){
+    int contador = 1;
+    while(num!=1){
+        if(num % 2 == 1) num = 3*num +1;
+        else num/=2;
+        contador++;
+    }
+    return contador;
+}
+
+int main() {
+    unsigned int star, end;
+    int max=0;
+    int temp;
+    while(scanf("%u %u ",&star,&end)==2){
+        cout<<star<<" "<<end<<" ";
+        for(int i=star; i<=end; i++){
+            temp = ciclos(i);
+            if(max < temp) max = temp;
+        }
+        cout<<max<<endl;
+        max=0;
+    }
+
 }
